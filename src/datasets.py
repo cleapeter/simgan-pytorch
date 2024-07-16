Correct image normalization

src/datasets.py
@@ -1,58 +1,48 @@
 # Import packages
-import importlib
+import numpy as np
 import pandas as pd
 import torch
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from PIL import Image
 from torch.utils.data import Dataset, DataLoader
-
-# Import own files
-import config
-
-# Reload own files
-importlib.reload(config)
+from torchvision import transforms
 
 
-class MNIST(Dataset):
+class Digits(Dataset):
     """Dataset class for input images, either real or synthetic.
 
     Args:
-        csv_file_path (str): path to csv input file
-        transform (str): transformation to use on images
+        csv_file_path (str): Path to the CSV input file.
+        transform (callable, optional): Transformation to apply to images.
     """
 
     def __init__(self, csv_file_path, transform=None):
-
+        # Get data
         self.data = pd.read_csv(csv_file_path)
         self.digits = self.data.iloc[:, 1:].values
         self.labels = self.data["label"].values
         self.transform = transform
 
-        if self.transform == "standard":
-            scaler = StandardScaler()
-            self.norm_digits = scaler.fit_transform(self.digits).reshape(
-                -1, config.img_height, config.img_width
-            )
-        elif self.transform == "minmax":
-            scaler = MinMaxScaler(feature_range=(-1, 1))
-            self.norm_digits = scaler.fit_transform(self.digits).reshape(
-                -1, config.img_height, config.img_width
-            )
+        # Infer image dimensions
+        total_pixels = self.digits.shape[1]
+        side_length = int(np.sqrt(total_pixels))  # Assuming square images
+        self.img_height = side_length
+        self.img_width = side_length
 
     def __getitem__(self, index):
+        digit = (
+            self.digits[index]
+            .reshape(self.img_height, self.img_width)
+            .astype(np.float32)
+        )
+        label = torch.tensor(self.labels[index], dtype=torch.long)
 
-        if self.transform is not None:
-            # Add channel dimension (1 channel for grayscale)
-            digit = (
-                torch.as_tensor(self.norm_digits[index])
-                .unsqueeze(dim=0)
-                .float()
-            )
-            label = torch.as_tensor(self.labels[index], dtype=torch.long)
+        if self.transform:
+            # Convert to PIL Image for applying torchvision transforms
+            digit = Image.fromarray(digit)
+            digit = self.transform(digit)
         else:
-            digit = self.digits[index].reshape(
-                -1, config.img_height, config.img_width
-            )
-            label = self.labels[index]
+            # Add channel dimension to match expected dimensions
+            digit = torch.tensor(digit).unsqueeze(dim=0)
 
         return digit, label
 
@@ -61,35 +51,44 @@ class MNIST(Dataset):
 
 
 def get_datasets(csv_file_paths, transform=None):
-    """Function creates PyTorch datasets for real and sythetic images.
+    """Creates PyTorch datasets for real and synthetic images.
 
     Args:
-        csv_file_path (str): path to csv input file
-        transform (str): transformation to use on images
+        csv_file_paths (list of str): List containing paths to CSV input files.
+        transform (callable, optional): Transformation to apply to images.
 
     Returns:
-        pytorch dataset: real dataset, synthetic dataset
+        tuple: (real_dataset, synthetic_dataset)
     """
+    if not isinstance(csv_file_paths, list) or len(csv_file_paths) != 2:
+        raise ValueError("csv_file_paths should be a list of two file paths.")
 
-    real_dataset = MNIST(csv_file_paths[0], transform=transform)
-    synthetic_dataset = MNIST(csv_file_paths[1], transform=transform)
+    real_dataset = Digits(csv_file_paths[0], transform=transform)
+    synthetic_dataset = Digits(csv_file_paths[1], transform=transform)
 
     return real_dataset, synthetic_dataset
 
 
 def get_dataloaders(csv_file_paths, batch_size, transform=None):
-    """Function creates PyTorch dataloaders for each dataset.
+    """Creates PyTorch dataloaders for each dataset.
+
+    Args:
+        csv_file_paths (list of str): List containing paths to CSV input files.
+        batch_size (int): Batch size for the dataloaders.
+        transform (callable, optional): Transformation to apply to images.
 
     Returns:
-        pytorch dataloader: real_loader, syn_loader
+        tuple: (real_loader, syn_loader)
     """
 
     real_dataset, synthetic_dataset = get_datasets(csv_file_paths, transform)
 
-    real_loader = DataLoader(real_dataset, batch_size=batch_size, shuffle=True)
+    real_loader = DataLoader(
+        real_dataset, batch_size=batch_size, shuffle=True, drop_last=True
+    )
 
     syn_loader = DataLoader(
-        synthetic_dataset, batch_size=batch_size, shuffle=True
+        synthetic_dataset, batch_size=batch_size, shuffle=True, drop_last=True
     )
 
     return real_loader, syn_loader
@@ -97,22 +96,32 @@ def get_dataloaders(csv_file_paths, batch_size, transform=None):
 
 if __name__ == "__main__":
     paths = ["data/input/real_mnist.csv", "data/input/synthetic_mnist.csv"]
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,)),
+        ]
+    )
 
-    # Test MNIST class
-    syn_dataset = MNIST(paths[1], transform="minmax")
+    # Test Digits class
+    syn_dataset = Digits(paths[1], transform=transform)
     print("Size of one synthetic image: ", syn_dataset[0][0].shape)
-
-    real_dataset = MNIST(paths[0], transform="minmax")
+    real_dataset = Digits(paths[0], transform=transform)
     print("Size of one real image: ", real_dataset[0][0].shape)
+    real_dataset_no_transform = Digits(paths[0], transform=None)
+    print(
+        "Size of one real image without transformation: ",
+        real_dataset_no_transform[0][0].shape,
+    )
 
     # Test get_datasets function
-    real_dataset, syn_dataset = get_datasets(paths, transform="minmax")
+    real_dataset, syn_dataset = get_datasets(paths, transform=transform)
     print("Number of all real images: ", len(real_dataset))
     print("Number of all synthetic images: ", len(syn_dataset))
 
     # Test get_dataloaders function
     real_dataloader, syn_dataloader = get_dataloaders(
-        paths, batch_size=16, transform="minmax"
+        paths, batch_size=16, transform=transform
     )
     print("Number of real image batches: ", len(real_dataloader))
     print("Number of synthetic image batches: ", len(syn_dataloader))